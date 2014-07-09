c0471e7530ef12d36e081681a1c5e3bf5cf2a950
maksimtrunov <trunovmaks@gmail.com>
2014-07-09 23:15:52 +0300

    Start

--- views/admin/users.php
+++ views/admin/users.php
@@ -24,9 +24,17 @@ $this->params['breadcrumbs'][] = $this->title;
 	?>
 	<?php echo GridView::widget([
 		'id' => 'users',
-		'layout' => "{summary}\n{items}\n{pager}",
-		'tableOptions' => ['class' => 'table table-striped table-bordered'],
+		'layout' => "{errors}\n{summary}\n{items}\n{pager}",
+		'tableOptions' => ['class' => 'table table-striped table-bordered table-hover'],
+		'captionOptions' => ['class' => 'lead'],
+		//'rowOptions' => ['class' => 'col-sm-12 col-md-12 col-lg-12'],
 		'caption' => 'Список пользователей',
 		'dataProvider' => $dataProvider,
+		'columns' => [
+			['class' => 'yii\grid\DataColumn', 'attribute' => 'id',],
+			['class' => 'yii\grid\DataColumn', 'attribute' => 'username', 'label' => 'Логин'],
+			['class' => 'yii\grid\DataColumn', 'attribute' => 'email', 'label' => 'Email'],
+			['class' => 'yii\grid\ActionColumn', 'controller' => 'admin',],
+		]
 	])?>
 </div>
