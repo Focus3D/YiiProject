4ee1df06af00ff531f8c822c78495a5ea689697d
Maksnet <trunovmaks@gmail.com>
2015-01-01 17:21:11 +0200

    process

--- views/file/index.php
+++ views/file/index.php
@@ -36,37 +36,38 @@ if (Yii::$app->session->getFlash('file')) : ?>
 	</div>
 </div>
 <div class="row">
-	<div class="panel panel-primary col-lg-12">
-		<div class="panel-body">
-			<table class="table table-hover table-bordered file-list">
-				<tr>
-					<td class="col-lg-1">#</td>
-					<td class="col-lg-6">Имя файла</td>
-					<td class="col-lg-2">Размер</td>
-					<td class="col-lg-3">Действие</td>
-				</tr>
-				<? $counter = 1; ?>
-				<? foreach ($files as $i => $file) : ?>
-					<tr>
-						<td class="col-lg-1"><?= $counter ?></td>
-						<td class="col-lg-6"><?= $file['name'] ?></td>
-						<td class="col-lg-2"><?= $file['size'] ?> kb</td>
-						<td class="col-lg-3">
-							<a href="<?= Url::to(['file/'. $file['id'] .'/get']) ?>">
-								<span class="glyphicon glyphicon-download" aria-hidden="true"></span>
-								Скачать
-							</a>
-							<?php if (Yii::$app->user->identity->username === 'admin') : ?>
-								<a href="<?= Url::to(['file/'. $file['id'] .'/delete']) ?>">
-									<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
-									Удалить
-								</a>
-							<?php endif ?>
-						</td>
-					</tr>
-					<? $counter++; ?>
-				<? endforeach ?>
-			</table>
-		</div>
-	</div>
+	<table class="table table-hover table-bordered file-list">
+		<caption>Список файлов</caption>
+		<thead>
+			<tr>
+				<td class="col-lg-1">#</td>
+				<td class="col-lg-6">Имя файла</td>
+				<td class="col-lg-2">Размер</td>
+				<td class="col-lg-3">Действие</td>
+			</tr>
+		</thead>
+		<? $counter = 1; ?>
+		<tbody>
+		<? foreach ($files as $i => $file) : ?>
+			<tr>
+				<td class="col-lg-1"><?= $counter ?></td>
+				<td class="col-lg-6"><?= $file['name'] ?></td>
+				<td class="col-lg-2"><?= $file['size'] ?> kb</td>
+				<td class="col-lg-3">
+					<a href="<?= Url::to(['file/'. $file['id'] .'/get']) ?>">
+						<span class="glyphicon glyphicon-download" aria-hidden="true"></span>
+						Скачать
+					</a>
+					<?php if (Yii::$app->user->identity->username === 'admin') : ?>
+						<a href="<?= Url::to(['file/'. $file['id'] .'/delete']) ?>">
+							<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
+							Удалить
+						</a>
+					<?php endif ?>
+				</td>
+			</tr>
+			<? $counter++; ?>
+		<? endforeach ?>
+		</tbody>
+	</table>
 </div>
